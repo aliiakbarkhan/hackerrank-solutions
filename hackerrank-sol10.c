cdc6a4ff3d2d68e2cc28b7165b411c228a55dd20
Ali Akbar Khan <56528027+ali363@users.noreply.github.com>
2024-01-15 15:07:24 +0530

    Create hackerrank-sol10.c
    Hackerrank C Programming Solution 10
    : 1D Arrays in C

---
+++ hackerrank-sol10.c
@@ -0,0 +1,22 @@
+//by ali akbar khan
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a[1000],i,n;
+        int sum = 0;
+    //  printf("enter size of arry:");
+    scanf("%d",&n);
+
+    for ( i = 0; i < n; i++)
+    {
+        //  printf("enter value of a[%d]",i);
+        scanf("%d",&a[i]);
+        sum = sum + a[i];
+    }
+    printf("%d",sum);
+
+
+
+    
+    return 0;
+}
